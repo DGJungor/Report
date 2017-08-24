d761689e5050777604312f685c37f7a90c3de925
DGJungor <568171152@qq.com>
2017-08-24 10:37:00 +0800

    重构 模块结构

---
+++ Config/MySQL.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/8/24
+ * Time: 10:23
+ */
+<?php
+
+//公共配置文件
+//数据库连接配置
+
+define("HOST","localhost");//主机名
+define("USER","root");//账户
+define("PASS","root");//密码
+define("DBNAME","report");//数据库
