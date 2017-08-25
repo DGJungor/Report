cc6ed6a354bca7694ec605a8881b14ed43bcae56
DGJungor <568171152@qq.com>
2017-08-25 15:06:40 +0800

    2017-08-25   15:07

--- App/Upload.php
+++ App/Upload.php
@@ -1,50 +1,49 @@
 <?php
-//include_once('inc/conn.php');
-//$file = $HTTP_POST_FILES['file']['name'];
-//$filetempname = $_FILES['file']['tmp_name'];
-//
-////自己设置的上传文件存放路径
-//	$filePath = '../upFile/';
-//	$str = "";
-//
-//	require_once 'PHPExcel.php';
-//	require_once 'PHPExcel\IOFactory.php';
-//	require_once 'PHPExcel\Reader\Excel2007.php';//excel 2007
-//
-//	$filename=explode(".",$file);//把上传的文件名以“.”好为准做一个数组。
-//	$time=date("Y-m-d-H-i-s");//去当前上传的时间
-//	$filename[0]=$time;//取文件名t替换
-//	$name=implode(".",$filename); //上传后的文件名
-//	$uploadfile=$filePath.$name;//上传后的文件名地址
-//
-//	//move_uploaded_file() 函数将上传的文件移动到新位置。若成功，则返回 true，否则返回 false。
-//  $result=move_uploaded_file($filetempname,$uploadfile);//假如上传到当前目录下
-//	if($result){ //如果上传文件成功，就执行导入excel操作
-//		$objReader = PHPExcel_IOFactory::createReader('Excel2007');
-//		$objPHPExcel = PHPExcel_IOFactory::load($uploadfile);
-//		$sheet = $objPHPExcel->getSheet(0);
-//		$highestRow = $sheet->getHighestRow(); // 取得总行数
-//		$highestColumn = $sheet->getHighestColumn(); // 取得总列数
-//
-//	//循环读取excel文件,读取一条,插入一条
-//		for($j=2;$j<=$highestRow;$j++){
-//			for($k='A';$k<=$highestColumn;$k++){
-//				$str .= iconv("UTF-8","gbk",$objPHPExcel->getActiveSheet()->getCell("$k$j")->getValue()).'\\';//读取单元格
-//			}
-//			$strs = explode("\\",$str);
-//			mysql_query("set names 'gb2312'");//这就是指定数据库字符集，一般放在连接数据库后面就系了
-//			$sql = "INSERT INTO report (number,month,data_1,data_2,data_3,data_4,data_5,data_6,data_7) VALUES('".$strs[0]."','".$strs[1]."','".$strs[2]."','".$strs[3]."','".$strs[4]."','".$strs[5]."','".$strs[6]."','".$strs[7]."','".$strs[8]."')";
-//			if(!mysql_query($sql)){
-//				return false;
-//			}
-//			$str = "";
-//			$p=$strs[0];
-//		}
-//		unlink($uploadfile); //删除上传的excel文件
-//		$msg = json_encode("$p");
-//	}else{
-//		$msg =json_encode("上传失败");
-//	}
-//	return  $msg;
-$msg = 123;
-return $msg;
+include_once('inc/conn.php');
+$file = $HTTP_POST_FILES['file']['name'];
+$filetempname = $_FILES['file']['tmp_name'];
+
+//自己设置的上传文件存放路径
+	$filePath = '../upFile/';
+	$str = "";
+
+	require_once 'PHPExcel.php';
+	require_once 'PHPExcel\IOFactory.php';
+	require_once 'PHPExcel\Reader\Excel2007.php';//excel 2007
+
+	$filename=explode(".",$file);//把上传的文件名以“.”好为准做一个数组。
+	$time=date("Y-m-d-H-i-s");//去当前上传的时间
+	$filename[0]=$time;//取文件名t替换
+	$name=implode(".",$filename); //上传后的文件名
+	$uploadfile=$filePath.$name;//上传后的文件名地址
+
+	//move_uploaded_file() 函数将上传的文件移动到新位置。若成功，则返回 true，否则返回 false。
+  $result=move_uploaded_file($filetempname,$uploadfile);//假如上传到当前目录下
+	if($result){ //如果上传文件成功，就执行导入excel操作
+		$objReader = PHPExcel_IOFactory::createReader('Excel2007');
+		$objPHPExcel = PHPExcel_IOFactory::load($uploadfile);
+		$sheet = $objPHPExcel->getSheet(0);
+		$highestRow = $sheet->getHighestRow(); // 取得总行数
+		$highestColumn = $sheet->getHighestColumn(); // 取得总列数
+
+	//循环读取excel文件,读取一条,插入一条
+		for($j=2;$j<=$highestRow;$j++){
+			for($k='A';$k<=$highestColumn;$k++){
+				$str .= iconv("UTF-8","gbk",$objPHPExcel->getActiveSheet()->getCell("$k$j")->getValue()).'\\';//读取单元格
+			}
+			$strs = explode("\\",$str);
+			mysql_query("set names 'gb2312'");//这就是指定数据库字符集，一般放在连接数据库后面就系了
+			$sql = "INSERT INTO report (number,month,data_1,data_2,data_3,data_4,data_5,data_6,data_7) VALUES('".$strs[0]."','".$strs[1]."','".$strs[2]."','".$strs[3]."','".$strs[4]."','".$strs[5]."','".$strs[6]."','".$strs[7]."','".$strs[8]."')";
+			if(!mysql_query($sql)){
+				return false;
+			}
+			$str = "";
+			$p=$strs[0];
+		}
+		unlink($uploadfile); //删除上传的excel文件
+		$msg = json_encode("$p");
+	}else{
+		$msg =json_encode("上传失败");
+	}
+	return  $msg;
+
--- Controllers/UploadController.php
+++ Controllers/UploadController.php
@@ -9,12 +9,29 @@
 //require_once 'App/PHPExcel.php';
 //require_once 'APP/PHPExcel\IOFactory.php';
 //require_once 'App/PHPExcel\Reader\Excel2007.php';//excel 2007
-require  'App/Test.class.php';
+require 'App/Test.class.php';
+require_once 'App/PHPExcel.php';
+require_once 'App/PHPExcel\IOFactory.php';
+require_once 'App/PHPExcel\Reader\Excel2007.php';//excel 2007
 
 class UploadController
 {
+    private $fileMag;                        //文件信息
+    private $filePath = "./upFile/";          //上传文件保存的路径
+//    private $allowtype = array('jpg','gif','png'); //设置限制上传文件的类型
+//    private $maxsize = 1000000;           //限制文件上传大小（字节）
+//    private $israndname = true;           //设置是否随机重命名文件， false不随机
+    private $originName;              //源文件名
+    private $tmpFileName;              //临时文件名
+//    private $fileType;               //文件类型(文件后缀)
+//    private $fileSize;               //文件大小
+//    private $newFileName;              //新文件名
+//    private $errorNum = 0;             //错误号
+//    private $errorMess="";             //错误报告消息
+
     public function __construct()
     {
+
 
     }
 
@@ -32,36 +49,54 @@ class UploadController
 
     public function UploadExcel()
     {
-        $file = $HTTP_POST_FILES['file']['name'];
-        $filetempname = $_FILES['file']['tmp_name'];
+
     }
 
     public function Test()
     {
+
+
+
+
         require('View/test.php');
         $view = new Index();
-        $t = new Test();
-
-
-        $data = $t->Index();
-        $view->display($data);
+        $view->display();
 
     }
 
+    /*
+     *
+     */
     public function Test2()
     {
+
+        $this->fileMag     = $_FILES;                               //获取文件信息
+        $this->tmpFileName = $_FILES['file']['tmp_name'];           //获取临时文件名
+        $this->originName  = $_FILES['file']['name'];               //获取源文件名
+        $time              = date("Y-m-d-H-i-s");           //获取当前时间
+
+        $filename    = explode(".", $this->originName);
+        $filename[0] = $time;                                         //取文件名t替换
+        $name        = implode(".", $filename);                 //上传后的文件名
+        $uploadfile  = $this->filePath . $name;                       //上传后的文件名地址
+        $result      = move_uploaded_file($this->tmpFileName, $uploadfile);          //上传到当前目录下   函数将上传的文件移动到新位置。若成功，则返回 true，否则返回 false。
+
+
+        $mag = $this->tmpFileName;
+        var_dump($mag);
+        var_dump($this->fileMag);
+        var_dump($filename);
+        var_dump($name);
+        var_dump($uploadfile);
+        var_dump($result);
+        die();
+
+
         require('View/test.php');
         $view = new Index();
-        $file = $HTTP_POST_FILES['file']['name'];
-        $filetempname = $_FILES['file']['tmp_name'];
-
-        $qwe = $_FILES;
-
-        var_dump($qwe);
-        die();
-        $data = $file;
         $view->display($data);
     }
 
 }
+
 ?>
